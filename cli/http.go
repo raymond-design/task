90d65b17a5bdc04b469c73ea68a898c3c001b3f6
raymond-design <raymondyan777@gmail.com>
2022-03-08 14:27:40 -0500

    basic cmds boilerplate

--- cli/http.go
+++ cli/http.go
@@ -1,1 +1,15 @@
 package cli
+
+import "net/http"
+
+type Client struct {
+	client *http.Client
+	uri    string
+}
+
+func NewClient(uri string) Client {
+	return Client{
+		client: &http.Client{},
+		uri:    uri,
+	}
+}
--- cli/switch.go
+++ cli/switch.go
@@ -1,1 +1,66 @@
 package cli
+
+import "fmt"
+
+type HttpClient interface {
+}
+
+type Switch struct {
+	client HttpClient
+	api    string
+	cmds   map[string]func() func(string) error
+}
+
+func createSwitch(uri string) Switch {
+	httpClient := NewClient(uri)
+
+	switch1 := Switch{
+		client: httpClient,
+		api:    uri,
+	}
+
+	switch1.cmds = map[string]func() func(string) error{
+		"create": switch1.create,
+		"edit":   switch1.edit,
+		"fetch":  switch1.fetch,
+		"delete": switch1.delete,
+		"health": switch1.health,
+	}
+
+	return switch1
+}
+
+func (s Switch) create() func(string) error {
+	return func(cmd string) error {
+		fmt.Println("create")
+		return nil
+	}
+}
+
+func (s Switch) edit() func(string) error {
+	return func(args string) error {
+		fmt.Println("switch")
+		return nil
+	}
+}
+
+func (s Switch) fetch() func(string) error {
+	return func(args string) error {
+		fmt.Println("fetch")
+		return nil
+	}
+}
+
+func (s Switch) delete() func(string) error {
+	return func(args string) error {
+		fmt.Println("delete")
+		return nil
+	}
+}
+
+func (s Switch) health() func(string) error {
+	return func(args string) error {
+		fmt.Println("health")
+		return nil
+	}
+}
