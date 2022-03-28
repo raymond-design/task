f842a92cd99b9a5576650ff077e48ad5e4925f1c
raymond-design <raymondyan777@gmail.com>
2022-03-28 00:51:12 -0400

    server signal listener

---
+++ api/server.go
@@ -0,0 +1,32 @@
+package api
+
+import (
+	"log"
+	"os"
+	"os/signal"
+)
+
+type Stopper interface {
+	Stop() error
+}
+
+func ListenForSignals(signals []os.Signal, apps ...Stopper) {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, signals...)
+	sig := <-c
+	log.Printf("received shutdown signal: %v\n", sig.String())
+
+	var errs []error
+	for _, app := range apps {
+		err := app.Stop()
+		if err != nil {
+			errs = append(errs, err)
+		}
+	}
+	var exitCode int
+	for _, err := range errs {
+		log.Printf("could not stop service due to: %v\n", err)
+		exitCode = 1
+	}
+	os.Exit(exitCode)
+}
