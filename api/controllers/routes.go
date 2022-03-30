a7cf1b7ad28ebc065b6f4006c30b78bef70c2279
raymond-design <raymondyan777@gmail.com>
2022-03-30 01:36:57 -0400

    ctx/parse controllers + task create handler

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -1,1 +1,16 @@
 package controllers
+
+// HTTP params
+const (
+	idParamName  = "id"
+	idsParamName = "ids"
+	idParam      = `{` + idParamName + `}:^[0-9]+$`
+	idsParam     = `{` + idsParamName + `}:[0-9]+(,[0-9]+)*`
+)
+
+type RemindersService interface {
+	creator
+	//editor
+	//fetcher
+	//deleter
+}
