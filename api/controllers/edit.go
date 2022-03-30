70974f8dc55cdc812a506e578d29210c99373ace
raymond-design <raymondyan777@gmail.com>
2022-03-30 19:48:27 -0400

    del, edit, fetch handler logic

--- api/controllers/edit.go
+++ api/controllers/edit.go
@@ -1,1 +1,46 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/raymond-design/task/api/json_transport"
+
+	"github.com/raymond-design/task/api/models"
+	"github.com/raymond-design/task/api/services"
+)
+
+type editor interface {
+	Edit(reminderBody services.ReminderEditBody) (models.Reminder, error)
+}
+
+func editReminder(service editor) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id, err := parseIDParam(r.Context())
+		if err != nil {
+			json_transport.SendError(w, err)
+			return
+		}
+		var body struct {
+			Title    string        `json:"title"`
+			Message  string        `json:"message"`
+			Duration time.Duration `json:"duration"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			json_transport.SendError(w, models.InvalidJSONError{Message: err.Error()})
+			return
+		}
+		reminder, err := service.Edit(services.ReminderEditBody{
+			ID:       id,
+			Title:    body.Title,
+			Message:  body.Message,
+			Duration: body.Duration,
+		})
+		if err != nil {
+			json_transport.SendError(w, err)
+			return
+		}
+		json_transport.SendJSON(w, reminder, http.StatusOK)
+	})
+}
