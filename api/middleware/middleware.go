34cad5580d332f5c0d127cefae18a801c170d10a
raymond-design <raymondyan777@gmail.com>
2022-03-30 01:13:52 -0400

    Middleware: middleware chains

--- api/middleware/middleware.go
+++ api/middleware/middleware.go
@@ -1,1 +1,28 @@
 package middleware
+
+import (
+	"net/http"
+)
+
+// creates a new instance of Middleware chains
+func New(ms ...func(h http.Handler) http.Handler) *Middleware {
+	return &Middleware{
+		functions: ms,
+	}
+}
+
+// HTTP router middleware type
+type Middleware struct {
+	functions []func(h http.Handler) http.Handler
+}
+
+// runs the request through the middleware chain, then serves it
+func (m *Middleware) Then(h http.Handler) http.Handler {
+	if h == nil {
+		h = http.DefaultServeMux
+	}
+	for i := range m.functions {
+		h = m.functions[len(m.functions)-1-i](h)
+	}
+	return h
+}
