3b1dd247099cd417cbf658228eda35fb5b066ff1
raymond-design <raymondyan777@gmail.com>
2022-03-30 22:14:51 -0400

    router server init

--- api/controllers/routes.go
+++ api/controllers/routes.go
@@ -1,5 +1,11 @@
 package controllers
 
+import (
+	"net/http"
+
+	"github.com/raymond-design/task/api/middleware"
+)
+
 // HTTP params
 const (
 	idParamName  = "id"
@@ -10,7 +16,26 @@ const (
 
 type RemindersService interface {
 	creator
-	//editor
-	//fetcher
-	//deleter
+	editor
+	fetcher
+	deleter
+}
+
+// RouterConfig represents router specific configuration
+type RouterConfig struct {
+	Service RemindersService
+}
+
+// NewRouter creates a new server (backend) application router
+func NewRouter(cfg RouterConfig) http.Handler {
+	r := RegexpMux{}
+	m := middleware.New(
+		middleware.HTTPLogger,
+	)
+	r.Get("/health", m.Then(health()))
+	r.Post("/reminders", m.Then(createReminder(cfg.Service)))
+	r.Get("/reminders/"+idsParam, m.Then(fetchReminders(cfg.Service)))
+	r.Delete("/reminders/"+idsParam, m.Then(deleteReminders(cfg.Service)))
+	r.Patch("/reminders/"+idParam, m.Then(editReminder(cfg.Service)))
+	return r
 }
--- api/server.go
+++ api/server.go
@@ -1,32 +1,71 @@
 package api
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"os"
-	"os/signal"
+	"net/http"
+	"time"
+
+	"github.com/raymond-design/task/api/controllers"
+	"github.com/raymond-design/task/api/models"
+	"github.com/raymond-design/task/api/services"
 )
 
-type Stopper interface {
-	Stop() error
+type Backend struct {
+	server  *http.Server
+	service *services.Reminders
 }
 
-func ListenForSignals(signals []os.Signal, apps ...Stopper) {
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, signals...)
-	sig := <-c
-	log.Printf("received shutdown signal: %v\n", sig.String())
+// initializes and creates a new server
+func New(addr string, service *services.Reminders) *Backend {
+	cfg := controllers.RouterConfig{Service: service}
+	router := controllers.NewRouter(cfg)
+	return &Backend{
+		server: &http.Server{
+			Addr:    addr,
+			Handler: router,
+		},
+		service: service,
+	}
+}
 
-	var errs []error
-	for _, app := range apps {
-		err := app.Stop()
-		if err != nil {
-			errs = append(errs, err)
+// starts the initialized
+func (b *Backend) Start() error {
+	log.Printf("application started on address %s\n", b.server.Addr)
+	err := b.service.Populate()
+	if err != nil {
+		return models.WrapError("could not initialize reminders service", err)
+	}
+
+	err = b.server.ListenAndServe()
+	if err == http.ErrServerClosed {
+		log.Println("http server is closed")
+		return nil
+	}
+	return err
+}
+
+// gracefully stops the server
+func (b *Backend) Stop() error {
+	timeout := 2 * time.Second
+	done, err := make(chan struct{}), make(chan error)
+
+	go func() {
+		log.Println("shutting down the http server")
+		if e := b.server.Shutdown(context.Background()); e != nil {
+			err <- models.WrapError("error on server shutdown", e)
 		}
+		close(done)
+	}()
+
+	select {
+	case <-done:
+		log.Println("application was shut down")
+		return nil
+	case e := <-err:
+		return e
+	case <-time.After(timeout):
+		return fmt.Errorf("shudown timeout of %v", timeout)
 	}
-	var exitCode int
-	for _, err := range errs {
-		log.Printf("could not stop service due to: %v\n", err)
-		exitCode = 1
-	}
-	os.Exit(exitCode)
 }
